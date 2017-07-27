2d22daea44750d61b050b316308c499374980159
Brian <briankimx91@gmail.com>
2017-07-27 11:27:17 -0700

    final project started

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import './app.css';
-import logo from './imgs/logo.svg';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="rotate"/>
-            <h1>Welcome to React!</h1>
-        </div>
+
     </div>
 );
 
