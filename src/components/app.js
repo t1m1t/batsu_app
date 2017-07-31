8e2f17b7636b442a5b5c02ac0ad1a3839f18b488
Tim <timothy.yi7@gmail.com>
2017-07-31 16:02:00 -0700

    added bryans events creation page proto

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,8 @@ import {BrowserRouter as Router,
     Link
 } from 'react-router-dom';
 import Home from './home';
-import Sign_Up from "./sign_up";
+import Sign_Up from './sign_up';
+import Events from './events';
 
 
 const App = () => (
@@ -12,9 +13,11 @@ const App = () => (
        <div className="topbar-menu">
            <Link className="topbar-links" to="/">Home</Link>
            <Link className="topbar-links" to="/sign_up">Sign Up</Link>
+           <Link className="topbar-links" to="/events">Events</Link>
 
            <Route exact path="/" component={Home} />
            <Route exact path="/sign_up" component={Sign_Up} />
+           <Route exact path="/events" component={Events}/>
        </div>
    </Router >
 )
