bb42e201390436fc9feabb4778273dd9c188991a
Jay Lim <jaylim106@gmail.com>
2017-08-04 13:42:38 -0700

    fixed somethings

--- src/components/app.js
+++ src/components/app.js
@@ -5,16 +5,17 @@ import {BrowserRouter as Router,
 } from 'react-router-dom';
 import Home from './home';
 import Sign_Up from './sign_up';
-import Event from './events';
-
+import Events from './events';
 const App = () => (
-   <Router>
-       <div className="topbar-menu">
-           <Route exact path="/" component={Home} />
-           <Route exact path="/sign_up" component={Sign_Up} />
-           <Route exact path="/events" component={Event}/>
-       </div>
-   </Router >
+    <Router>
+        <div className="topbar-menu">
+            <Link className="topbar-links" to="/">Home</Link>
+            <Link className="topbar-links" to="/sign_up">Sign Up</Link>
+            <Link className="topbar-links" to="/events">Events</Link>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/sign_up" component={Sign_Up} />
+            <Route exact path="/events" component={Events}/>
+        </div>
+    </Router >
 )
-
 export default App;
